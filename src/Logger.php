a20a8af859a51cd90a9d1a9cc74fa1c005c8ce5c
Tim <git@nztm.net>
2015-05-28 07:35:25 +1200

    Add requestInfo() method to Logger

--- src/Logger.php
+++ src/Logger.php
@@ -1,18 +1,21 @@
 <?php namespace NZTim\Logger;
 
 use App;
+use Auth;
 use NZTim\Logger\Handlers\Handler;
 use Exception;
 
 class Logger
 {
     protected $handlers = [];
+    protected $request;
 
     public function __construct()
     {
         $this->handlers[] = App::make('NZTim\Logger\Handlers\FileHandler');
         $this->handlers[] = App::make('NZTim\Logger\Handlers\EmailHandler');
         $this->handlers[] = App::make('NZTim\Logger\Handlers\PapertrailHandler');
+        $this->request = App::make('Illuminate\Http\Request');
     }
 
     /**
@@ -73,5 +76,23 @@ class Logger
         $ds = DIRECTORY_SEPARATOR;
         $filename = storage_path() . "{$ds}logs{$ds}fatal-logger-errors.log";
         file_put_contents($filename, $message, FILE_APPEND);
+    }
+
+    public function requestInfo() 
+    {
+        $info = [];
+        $info['ip'] = $this->request->getClientIp();
+        $info['method'] = $this->request->server('REQUEST_METHOD');
+        $info['url'] = $this->request->url();
+        $info['user'] = Auth::user();
+        $input = $this->request->all();
+        if(isset($input['password'])) {
+            unset($input['password']);
+        }
+        if(isset($input['password_confirmation'])) {
+            unset($input['password_confirmation']);
+        }
+        $info['input'] = $input;
+        return $info;
     }
 }
